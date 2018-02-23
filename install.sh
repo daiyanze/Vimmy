91b0e4c982dd30075ddacea90a875d8dcd432c3e
Yanze Dai <ginoalex8964@yahoo.com>
2018-02-23 22:34:00 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# replace .vimrc
+yes | cp -rf ~/.vimmy/.vimrc ~/.vimrc
+
+# install plugins
+vim -c PlugInstall | qall
