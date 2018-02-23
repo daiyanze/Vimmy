bddca4bb0dcdc884801474f4a465faeafd1bbfcb
Yanze Dai <ginoalex8964@yahoo.com>
2018-02-23 23:01:26 +0900

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,7 @@
 # remove Vim related files and folder
 echo -e remove "\e[31m\.vim\e[m"
-rm -rf .vim
+rm .vim
 echo -e remove "\e[31m\.vimmy\e[m"
-rm -rf .vimmy
+rm .vimmy
 echo -e remove "\e[31m\.vimrc\e[m"
+rm .vimrc
